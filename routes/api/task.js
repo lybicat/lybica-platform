3cedc2a74c9a7b9f591610ab6549aba1c513c4cb
Zhang Yu <yu.3.zhang@nokia.com>
2015-11-06 12:59:23 +0800

    use promise to flattern the code

--- routes/api/task.js
+++ routes/api/task.js
@@ -23,25 +23,23 @@ module.exports = {
     },
     '/api/tasks/:id/result': {
         post: function(req, res, next) {
-            Task.findById(req.params.id, function(err, task) {
-                if (err) {
-                    return next(err);
-                } else if (task === null) {
+            Task.findById(req.params.id)
+            .then(function(task) {
+                if (task === null) {
                     res.send(404, {'err': 'task ' + req.params.id + ' not found'});
                     return next();
-                } else {
-                    task.result = req.body;
-                    task.markModified('result');
-                    if (task.result.passed === true) {
-                        task.passed = true;
-                    }
-                    task.done = true;
-                    task.save(function(err, t) {
-                        if (err) return next(err);
-                        res.send(200, {});
-                        return next();
-                    });
                 }
+                task.result = req.body;
+                task.markModified('result');
+                if (task.result.passed === true) {
+                    task.passed = true;
+                }
+                task.done = true;
+                task.save(function(err, t) {
+                    if (err) return next(err);
+                    res.send(200, {});
+                    return next();
+                });
             });
         }
     }

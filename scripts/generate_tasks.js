8c85be97ee5957b70eed8f55106dba1eaa3e5b55
Zhang Yu <yu.3.zhang@nokia.com>
2015-11-06 13:59:01 +0800

    add data generating script

---
+++ scripts/generate_tasks.js
@@ -0,0 +1,15 @@
+var restify = require('restify');
+var client = restify.createJsonClient({
+    url: 'http://127.0.0.1'
+});
+
+var build = process.argv[2];
+var caseset = process.argv[3];
+var device = process.argv[4];
+var actions = process.argv[5].split(';');
+
+client.post('/api/tasks', {build: build, caseset: caseset, device: device, actions: actions}, function(err, res, req, obj) {
+    if (err === null) {
+        console.log('new task created!');
+    }
+});

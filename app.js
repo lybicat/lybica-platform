0ac146c6b6f0b54a355275d6d4ecfec6c2d2cb3e
Zhang Yu <feiyuw@gmail.com>
2015-11-15 09:57:30 +0800

    use 2 space as tab indention

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ var config = require('./config');
 var restifyRoutes = require('restify-routes');
 
 var server = restify.createServer({
-    name: 'lybica',
-    version: '1.0.0'
+  name: 'lybica',
+  version: '1.0.0'
 });
 
 server.use(restify.acceptParser(server.acceptable));
@@ -13,8 +13,8 @@ server.use(restify.queryParser());
 server.use(restify.bodyParser());
 
 server.use(function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    return next();
+  res.header('Access-Control-Allow-Origin', '*');
+  return next();
 });
 
 restifyRoutes.set(server, __dirname + '/routes');
@@ -22,5 +22,5 @@ restifyRoutes.set(server, __dirname + '/routes');
 mongoose.connect(config.DB_URL);
 
 server.listen(config.PORT, function() {
-    console.log('%s listening at %s', server.name, server.url);
+  console.log('%s listening at %s', server.name, server.url);
 });
